208b4658502d01777adf21717d41956ee3f5c7f7
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-05-21 10:16:55 +0300

    View Tool initial commit

---
+++ lib/perkin_view_tool.rb
@@ -0,0 +1,7 @@
+require "perkin_view_tool/version"
+require "perkin_view_tool/renderer"
+
+module PerkinViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/perkin_view_tool/version.rb
@@ -0,0 +1,3 @@
+module PerkinViewTool
+  VERSION = "0.1.0"
+end
